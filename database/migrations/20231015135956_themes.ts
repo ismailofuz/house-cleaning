6c97b7eea76a0f11e690bfd4b553a67e9eac3b34
ismailofuz <sirojiddin.ismoilov7181@gmail.com>
2023-10-18 17:41:53 +0500

    chore: update themes migration

--- database/migrations/20231015135956_themes.ts
+++ database/migrations/20231015135956_themes.ts
@@ -9,7 +9,7 @@ export async function up(knex: Knex): Promise<void> {
         table
             .integer('section_id')
             .references('id')
-            .inTable('sunject_sections');
+            .inTable('subject_sections');
         table.timestamps(true, true);
     });
 }
